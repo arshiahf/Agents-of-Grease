326ac7a12982a0656330c5635dd7cf5cbd847ff2
Matthew Taurone <arshiahf@gmail.com>
2021-07-14 10:15:41 -0400

    Put in skeleton code for application and main

---
+++ source/application.py
@@ -0,0 +1,22 @@
+import sprite
+import tilemap
+import pygame
+
+class Application(width, height):
+
+    def __init__(self, width, height):
+
+        self.width = width
+        self.height = height
+
+    def pull_sprites(self, sprite_list):
+
+        return None
+
+    def set_map(self, map_file):
+
+        return None
+
+    def run(self):
+
+        return None
---
+++ source/main.py
@@ -0,0 +1,9 @@
+import application
+
+game_starting_map = None
+game_sprites = None
+
+app = application.Application(800, 600)
+app.pull_sprites(game_sprites)
+app.set_map(game_starting_map)
+app.run()
